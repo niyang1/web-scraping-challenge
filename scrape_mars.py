0f9e8d5defd65417bb78ad9a3bab99475dd777e7
Sika Inyang <ninyang1@gmail.com>
2021-03-29 22:42:24 -0500

    first commit

---
+++ scrape_mars.py
@@ -0,0 +1,64 @@
+# Dependencies
+from bs4 import BeautifulSoup 
+import requests
+import pandas as pd
+
+from splinter import Browser
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+    # Set up Splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=True) 
+
+def scrape_mars():
+
+    # URL of page to be scraped
+    url = 'https://mars.nasa.gov/news/'
+    # Retrieve page with the requests module
+    response = requests.get(url)
+    # Create BeautifulSoup object; parse with 'html.parser'
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    news_title = soup.find("div",class_="content_title").text.strip()
+    news_p = soup.find("div",class_="rollover_description_inner").text.strip()
+
+    url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
+    browser.visit(url)
+    html = browser.html
+    img_soup = BeautifulSoup(html, 'html.parser')
+    urlb = img_soup.find('img', class_='headerimage')['src']
+    featured_image_url = f"https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{urlb}"
+    
+    url = "https://space-facts.com/mars/"
+    facts_table = pd.read_html(url)[0].to_html()
+
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+    html = browser.html
+    hemi_soup = BeautifulSoup(html, 'html.parser')
+    banners_tag = hemi_soup.find_all('h3')
+    banners = [x.text for x in banners_tag]
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+
+    hemispheres = []
+    for i in range(len(banners)):
+        hemisphere = {}
+        browser.visit(url)
+        browser.find_by_css('h3')[i].click()    
+        hemisphere[banners[i]] = browser.find_by_text('Sample')['href']        
+        hemispheres.append(hemisphere)        
+        browser.back()
+
+    # Store data in a dictionary
+    mars_data = {
+        "news_title": news_title,
+        "news_p": news_p,
+        "max_temp": max_temp
+    }
+
+    # Close the browser after scraping
+    browser.quit()
+
+    # Return results
+    return mars_data
